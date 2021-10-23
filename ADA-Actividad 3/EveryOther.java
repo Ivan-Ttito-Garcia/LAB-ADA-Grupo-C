5037f15e019b7b0be91ef744b0dca09df28c38a1
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-10-23 18:49:03 -0500

    Se creo la clase EveryOther la cual contiene un metodo que muestra la suma de los numeros de indice par con cada uno de los elementos del arreglo. Se añadio un caso de prueba y ademas se muestra la complejidad del algoritmo

---
+++ ADA-Actividad 3/EveryOther.java
@@ -0,0 +1,27 @@
+
+public class EveryOther {
+	public static void main(String[] arg) {
+		
+		//CASO DE PRUEBA
+		int[] arr = {2,3,4,5};
+		everyOther(arr);
+	}
+	
+	/*
+	 Metodo que muestra la suma de los numeros de indice par
+	 con cada uno de los elementos del arreglo.
+	 
+	 Dado que contiene un for anidado la complejidad es de n^2
+	 */
+	public static void everyOther(int[] arr) {
+		for(int i = 0 ; i < arr.length ; i++)
+			if(i % 2 == 0) {
+				for(int j = 0 ; j< arr.length ; j++)
+					System.out.println(arr[i]+arr[j]);
+				System.out.println();
+			}
+		
+				
+		
+	}
+}

cb5d72c07bea0cbb94b5f02643f35813be18b9b6
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-10-23 19:07:21 -0500

    Se creo la clase TwoSum que contiene un metodo que retorna true o false si dos de sus elementos de diferente posicion suman 10. Se añadio el caso de prueba y se calculo su complejidad

---
+++ ADA-Actividad 3/TwoSum.java
@@ -0,0 +1,26 @@
+
+public class TwoSum {
+	public static void main(String[] arg) {
+		
+		//CASO DE PRUEBA
+		int[] arr = {2,3,5,4,6};
+		System.out.println(twoSum(arr));
+	}
+	
+	/*
+	 Metodo que recibe un arreglo y retorna true o false
+	 dependiendo si la suma de 2 terminos de diferente 
+	 posicion en el arreglo suman 10
+	 
+	 En el algoritmo podemos apreciar un for anidado lo que
+	 nos dara una complejidad de n^2
+	 */
+	public static boolean twoSum(int[] arr) {
+		for(int i = 0 ; i < arr.length ; i++)
+			for(int j = 0 ; j < arr.length ; j++)
+				if( (i != j) && (arr[i] + arr[j] == 10))
+					return true;
+		return false;
+	}
+
+}

2d7e8bceb120555e6b2c9198716b831bac37d7c1
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-10-30 10:25:13 -0500

    Se implemento el ejercicio 8 y al igual que el enterior ejercicio se explico detalladamente su complejidad, resultando tambien logaritmica

---
+++ ADA-Actividad 4  26-10-2021/Ejercicio8_Logaritmica.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio8_Logaritmica {
+	public static void main(String[] arg) {
+		
+		/*
+		 *  En este algoritmo notamos que ahora inicialmente i 
+		 *  toma el valor de un n ingresado, y este se va dividiendo 
+		 *  entre 2 en cada repeticion hasta que tome un valor inferior a 1.
+		 *  Suponiendo que:
+		 *  K es el numero de repeticiones.
+		 *  i(final) = n / 2 / 2 / 2 ...
+		 *  			  [.........]
+		 *  				k veces
+		 *  Entonces i = n / (2 ^ k)
+		 *  Para salir del for, i < 1, reemplazando:
+		 *  =>    n / (2 ^ k) < 1 
+		 *  =>    n < 2 ^ k
+		 *  =>    log 2 (n) < k
+		 *  Entonces su comlpejidad sería de        O(log 2 (n))
+		 *  								 (logaritmo en base 2 de n)
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Ingrese un número");
+		int n = sc.nextInt();
+		
+		for(int i = n ; i >= 1 ; i = i / 2)
+			System.out.println(i);
+		
+	}
+}

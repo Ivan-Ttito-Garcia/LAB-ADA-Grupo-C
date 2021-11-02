ddcf4cc196ac5832f93f7fc25772c62c4e040397
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-02 16:17:01 -0500

    Se dejo pasar la complejidad por accidente ;v. Ya se corrigio, se añadio un comentario explicando la complejidad

--- ADA-Actividad 3/GreatestNumber.java
+++ ADA-Actividad 3/GreatestNumber.java
@@ -9,6 +9,11 @@ public class GreatestNumber {
  	}
 	
 	//Método que recibe un arreglo y retorna el mayor de sus elementos...
+	//La comlplejidad del algoritmo es lineal, pues se recorre el
+	//arreglo una sola vez y las sentencias dentro del for tambien 
+	//tienen una complejidad lineal, entonces: quitandlo las constantes
+	//sería de O(n)
+	
 	public static int greatestNumber(int[] arr) {
 		int mayor = arr[0];
 		for(int i = 1 ; i <arr.length ; i++)

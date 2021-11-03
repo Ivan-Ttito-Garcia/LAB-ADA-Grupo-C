a3e8fd1c38938ad39e608a7c9dc38d6cbca55477
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-03 18:50:25 -0500

    Se creo la clase binary search i se implemento el codigo

---
+++ ADA-Actividad 6  02-11-2021/Binary_Search.java
@@ -0,0 +1,36 @@
+
+public class Binary_Search {
+	
+	public static void main(String[] arg) {
+		
+		int[] arr = {2,5,9,11,15,20,32,41,45,50,55,61,80};
+		System.out.println("==> "+BinarySearch(arr,141));
+		
+	}
+	
+	public static int BinarySearch(int[] arr, int targed) {
+		
+		int l = 0;
+		int r = arr.length - 1;
+		
+		int mid;
+		
+		while (l <= r) {
+			
+			mid = l + (r - l) / 2;
+			
+			if ( arr[mid] == targed )
+				return mid;
+			
+			else if ( arr[mid] < targed )
+				l = mid + 1;
+			
+			else 
+				r = mid-1;
+			
+		}
+		
+		return -1;
+	}
+	
+}

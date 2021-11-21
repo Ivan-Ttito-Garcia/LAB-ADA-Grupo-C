d4354fcae662174a82706345aa9ab72e3fbf043a
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-21 13:54:10 -0500

    Se comentaron algunas explicaciones

--- ADA-Actividad 8  16-11-2021/InterviewWait.java
+++ ADA-Actividad 8  16-11-2021/InterviewWait.java
@@ -6,11 +6,15 @@ public class InterviewWait {
 		
 		Deque<Integer> deque = new ArrayDeque<Integer>();
 		
+		//Caso de prueba
 		int[] input = {4,-1,5,2,3};
+		
+		//Implementacion del algoritmo...
 		int output = 0;
 		for(int x : input)
 			deque.offerLast(x);
 		
+		//Se compara almacenando el resultado de la suma hasta que uno de los extremos sea -1.
 		while(deque.getFirst() != -1 && deque.getLast() != -1) {
 			if(deque.getFirst() > deque.getLast())
 				output += deque.removeLast();
@@ -19,6 +23,7 @@ public class InterviewWait {
 				output += deque.removeFirst();
 		}
 		
+		//Resultado
 		System.out.println("Output = "+output);
 		
 	}

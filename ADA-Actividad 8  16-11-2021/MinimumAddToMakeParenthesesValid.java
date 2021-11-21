6c0777b70c2bfc4005848838632660ab52f825da
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-21 13:57:33 -0500

    Se comentaron algunos aspectos importantes

--- ADA-Actividad 8  16-11-2021/MinimumAddToMakeParenthesesValid.java
+++ ADA-Actividad 8  16-11-2021/MinimumAddToMakeParenthesesValid.java
@@ -6,19 +6,26 @@ public class MinimumAddToMakeParenthesesValid {
 		
 		Deque<String> stack = new ArrayDeque<String>();
 		
+		//Casos de prueba..
 		//String aux = "())";
 		//String aux = "(((";
 		//String aux = "(()))(";
 		String aux = "()))((";
 		
+		//Implementacion del algoritmo: Se inserta y evalua los elementos uno a uno...
 		for(int i = 0 ; i < aux.length() ; i++) {
 			stack.push(String.valueOf(aux.charAt(i)));
 			evaluar(stack);
 		}
 		
-		System.out.println("Resultado = "+stack.size());
+		//Salida
+		System.out.println("Output = "+stack.size());
 	}
 	
+	/*
+	 * Este metodo evalua si algun parentesis cerrado tiene uno abierto anteriormente,
+	 * si es asi elimina los dos parentesis, caso contrario no sucede nada.
+	 */
 	public static void evaluar(Deque<String> stack) {
 		
 		if(stack.peek().equals(")") && stack.size() != 1) {

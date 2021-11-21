fd9bf47de31275077eb34b88dff29fde0c7006e4
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-21 09:10:46 -0500

    Se agrego el primer ejercicio, se culmino los casos de prueba, esta concluido, pero falta la explicacion...

---
+++ ADA-Actividad 8  16-11-2021/EvaluateReversePolishNotation.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class EvaluateReversePolishNotation {
+	
+	public static void main(String[] arg) {
+		
+		Deque<String> stack = new ArrayDeque<String>();
+		//String[] aux = {"4","13","5","/","+"};
+		//String[] aux = {"2","1","+","3","*"};
+		String[] aux = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+		
+		for(String x : aux) {
+			stack.push(x);
+			evaluar(stack);
+		}
+		/*
+		stack.push("4");
+		evaluar(stack);
+		stack.push("13");
+		evaluar(stack);
+		stack.push("5");
+		evaluar(stack);
+		stack.push("/");
+		evaluar(stack);
+		stack.push("+");
+		evaluar(stack);
+		*/
+		
+		int resultado = Integer.parseInt(stack.pop());
+		
+		System.out.println("Resultado = "+resultado);
+	}
+	
+	public static void evaluar(Deque<String> stack) {
+		
+		if(stack.peek()=="+" || stack.peek()=="-" || stack.peek()=="*" || stack.peek()=="/") {
+			
+			String signo = stack.pop();
+			
+			int op1 = Integer.parseInt(stack.pop());
+			int op2 = Integer.parseInt(stack.pop());
+			
+			if(signo=="+") 
+				stack.push(""+(op2+op1));
+				
+			if(signo=="-") 
+				stack.push(""+(op2-op1));
+			
+			if(signo=="*") 
+				stack.push(""+(op2*op1));
+			
+			if(signo=="/") 
+				stack.push(""+(op2/op1));
+			
+		}
+		
+	}
+
+}

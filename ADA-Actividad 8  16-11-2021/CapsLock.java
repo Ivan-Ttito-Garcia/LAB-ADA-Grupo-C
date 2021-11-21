b2b745ec468f92ea565dd379e9752232ca70f352
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-21 13:47:24 -0500

    Se describieron algunas partes importantes

--- ADA-Actividad 8  16-11-2021/CapsLock.java
+++ ADA-Actividad 8  16-11-2021/CapsLock.java
@@ -5,7 +5,15 @@ public class CapsLock {
 	public static void main(String[] arg) {
 		Deque<String> stack = new ArrayDeque<String>();
 		
+		//Caso de prueba
 		String input = "abc$d@ef$@g$";
+		
+		/*
+		 * Implementacion del algoritmo:
+		 * Se crea la variable output que ira almacenando la cadena formada
+		 * cada vez que se vacia el bufer, mayus se actualiza cada vez que 
+		 * haya un @ para ver si el bufer sera en mayusculas o minusculas.
+		 */
 		String output = "";
 		boolean mayus = false;
 		
@@ -22,10 +30,16 @@ public class CapsLock {
 			
 		}
 		
+		//Salida
 		System.out.println(output);
 		
 	}
 	
+	/*
+	 * Cada vez que se vaya a vaciar el bufer se llamara a este metodo,
+	 * se ingresa previamente el valor de mayus, para ver si debemos 
+	 * retornar en mayusculas o minusculas.
+	 */
 	public static String vaciarBufer(Deque<String> stack, boolean mayus) {
 		
 		String aux = "";

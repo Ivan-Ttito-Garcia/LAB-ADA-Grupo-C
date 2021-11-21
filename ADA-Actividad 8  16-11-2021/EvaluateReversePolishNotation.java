f6c357abb7a531869f8f113d9d8c78209f5c11d8
Ivan-Ttito-Garcia <javier71_2001@outlook.com>
2021-11-21 13:52:19 -0500

    Se hizo la explicacion de algunas partes importantes del algoritmo...

--- ADA-Actividad 8  16-11-2021/EvaluateReversePolishNotation.java
+++ ADA-Actividad 8  16-11-2021/EvaluateReversePolishNotation.java
@@ -6,21 +6,36 @@ public class EvaluateReversePolishNotation {
 		
 		Deque<String> stack = new ArrayDeque<String>();
 		
+		
+		/*
+		 * Los casos de prueba se guardaron en arreglos para no tener que insertar los valores
+		 * uno a uno a la hora de ejecutar el programa, solo se hizo por motivos de facilidad...
+		 */
+		
+		//Casos de prueba
 		//String[] aux = {"4","13","5","/","+"};
 		//String[] aux = {"2","1","+","3","*"};
 		String[] aux = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
 		
+		
+		/*
+		 * Ejecucion del algoritmo: Se inserta uno a uno los elementos y
+		 * se va evaluando a lo largo del proceso...
+		 */
 		for(String x : aux) {
 			stack.push(x);
 			evaluar(stack);
 		}
 		
-		
+		//RESULTADO: Output
 		int resultado = Integer.parseInt(stack.pop());
-		
-		System.out.println("Resultado = "+resultado);
+		System.out.println("Output = "+resultado);
 	}
 	
+	/*
+	 * Este metodo es el encargado de operar los elemento cada vez que se encuentre
+	 * un signo... Se hace la respectiva operacion y se almacena el resultado.
+	 */
 	public static void evaluar(Deque<String> stack) {
 		
 		if(stack.peek()=="+" || stack.peek()=="-" || stack.peek()=="*" || stack.peek()=="/") {
